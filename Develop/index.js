d49a32bc5826317081e1f21f1b9955a80ba0544d
Richard Mercado <RichardFM291@gmail.com>
2021-06-19 16:24:41 -0400

    adding middleware, trying to get and  post api

--- Develop/index.js
+++ Develop/index.js
@@ -5,23 +5,29 @@ const notesJSON = require("./db/db.json")
 console.log(notesJSON);
 const app = express();
 const PORT = 3000;
+// json middleware
+app.use(express.json())
+app.use(express.urlencoded())
 
 // serve static 
 app.use(express.static("public"));
-// json middleware
+
 // url encoded middleware
 
 // API routes//(save/rewrite and load)
+// get API notes
 app.get("/api/notes", (req, res) => {
     console.log("/api/notes");
     res.json(notesJSON)
 });
-// get API notes
-
 // POST API notes
+app.post("/api/notes", (req, res) => {
+    notesJSON.push(req.body)
+    res.json(notesJSON.length -1)
+})
 
 //html routes
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")))
-
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+app.get("*", (req, res) => res.send("404"));
 // start
 app.listen(PORT, () => console.log ("Server Running on http://localhost:3000"))
